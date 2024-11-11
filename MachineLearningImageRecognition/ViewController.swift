cf93c29886e136e8f0b1860e43942d88c222dc2f
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-11-11 14:08:34 +0300

    feat: add image picker functionality for photo selection
    # Implemented UIImagePickerController to allow the user to select an image from the photo library.
    # Display selected image in UIImageView

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var resultLabel: UILabel!
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 

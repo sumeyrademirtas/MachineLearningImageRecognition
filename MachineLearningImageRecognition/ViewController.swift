50fc790e924e4409ea264bd558c4c6e303da5089
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-11-11 16:41:12 +0300

    feat: perform image recognition asynchronously using VNImageRequestHandler
    # added VNImageRequestHandler to process CIImage asynchronously
    # executed image classification request on a background thread with userInteractive QoS
    # error handling added for VNImageRequestHandler execution

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -5,38 +5,32 @@
 //  Created by Sümeyra Demirtaş on 11/11/24.
 //
 
-import UIKit
 import CoreML
+import UIKit
 import Vision // CoreML ile Image Recognition icin kullandigimiz yardimci kutuphane
 
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     var chosenImage = CIImage()
-
-    @IBOutlet weak var resultLabel: UILabel!
-   
-    @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet var resultLabel: UILabel!
     
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func changeClicked(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated: true, completion: nil)
-        
     }
     
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         imageView.image = info[.originalImage] as? UIImage
-        self.dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
         
         if let ciImage = CIImage(image: imageView.image!) {
             chosenImage = ciImage
@@ -46,33 +40,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func recognizeImage(image: CIImage) {
-        
         // 1) Request
         // 2) Handler
+        
+        resultLabel.text = "Loading..."
         let MobileNetConfig = MLModelConfiguration()
         if let model = try? VNCoreMLModel(for: MobileNetV2(configuration: MobileNetConfig).model) {
-            let request = VNCoreMLRequest(model: model) { (vnrequest, error) in
+            let request = VNCoreMLRequest(model: model) { vnrequest, _ in
                 
-                if let results = vnrequest.results as? [VNClassificationObservation] { //gorsel analizinin sonucunda uretilen bir siniflandirma
+                if let results = vnrequest.results as? [VNClassificationObservation] { // gorsel analizinin sonucunda uretilen bir siniflandirma
                     if results.count > 0 {
-                        
                         let topResult = results.first
                         
                         DispatchQueue.main.async {
-                            
                             let confidenceLevel = (topResult?.confidence ?? 0) * 100
-                            self.resultLabel.text = "\(confidenceLevel)% it's \(topResult?.identifier)"
+                            let rounded = Int(confidenceLevel * 100) / 100
+                            self.resultLabel.text = "\(rounded)% it's \(topResult!.identifier)"
                             
-                            
-                            
-//                            self.resultLabel.text = "\(topResult!.identifier) (\(topResult!.confidence * 100)%)"
+                            //                            self.resultLabel.text = "\(topResult!.identifier) (\(topResult!.confidence * 100)%)"
                         }
                     }
                 }
-                    
+            }
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([request])
+                } catch {
+                    print("error")
+                }
             }
         }
     }
-    
 }
-
